4ebb6ab8c3fcbc8a3b70a8e58cfdbe0053b37804
Rahil Ahmad <rahilahmad1258@gmail.com>
2022-03-15 19:24:42 +0500

    Add native base theme

--- App.tsx
+++ App.tsx
@@ -3,32 +3,24 @@ import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native'
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NativeBaseProvider } from 'native-base'
+
 import HomeContainer from './src/containers/HomeContainer';
+import { customTheme } from './src/common/themes';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const MyTheme = {
-    dark: false,
-    colors: {
-      primary: 'rgb(255, 45, 85)',
-      background: 'rgb(242, 242, 242)',
-      card: 'rgb(255, 255, 255)',
-      text: 'rgb(28, 28, 30)',
-      border: 'rgb(199, 199, 204)',
-      notification: 'rgb(255, 69, 58)',
-    },
-  };
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NavigationContainer theme={MyTheme}>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeContainer} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </View >
+    <NativeBaseProvider theme={customTheme}>
+      <View style={styles.container}>
+        <StatusBar barStyle={'dark-content'} />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeContainer} options={{ headerShown: false }} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </View >
+    </NativeBaseProvider>
   );
 };
 
