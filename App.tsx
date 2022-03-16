812a13c6687f28b0371e904406578c1a006e8f8e
PraneethGunas <praneethgunas404@gmail.com>
2022-03-16 18:10:51 +0530

    Added localisation

--- App.tsx
+++ App.tsx
@@ -1,33 +1,20 @@
 import React from 'react';
-import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { NativeBaseProvider } from 'native-base'
-
 import HomeContainer from './src/containers/HomeContainer';
-import { customTheme } from './src/common/themes';
+import Providers from './src/Providers';
 
 const App = () => {
   const Stack = createNativeStackNavigator();
   return (
-    <NativeBaseProvider theme={customTheme}>
-      <View style={styles.container}>
-        <StatusBar barStyle={'dark-content'} />
-        <NavigationContainer>
-          <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeContainer} options={{ headerShown: false }} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View >
-    </NativeBaseProvider>
+    <Providers>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeContainer} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Providers>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
