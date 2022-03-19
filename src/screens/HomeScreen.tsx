e4de552d10bdc1256cc6c086391a49b8b274998e
PraneethGunas <praneethgunas404@gmail.com>
2022-03-19 22:35:28 +0530

    Updated babel and metro config

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, View } from 'react-native';
-import { Text, useColorMode, useColorModeValue } from 'native-base'
+import {SafeAreaView, StyleSheet, View} from 'react-native';
+import {Text, useColorMode, useColorModeValue} from 'native-base';
+import Broh from 'assets/images/beardo.svg';
 
 const HomeScreen = () => {
-  const {
-    colorMode,
-    toggleColorMode
-  } = useColorMode();
+  const {colorMode, toggleColorMode} = useColorMode();
 
   return (
     <SafeAreaView style={styles.sectionContainer}>
       <View>
-        <Text style={styles.sectionTitle} onPress={toggleColorMode} color={useColorModeValue("light.blue", "dark.black")} >{colorMode}</Text>
+        <Text
+          style={styles.sectionTitle}
+          onPress={toggleColorMode}
+          color={useColorModeValue('light.blue', 'dark.black')}>
+          {colorMode}
+        </Text>
+        <Broh />
       </View>
-    </SafeAreaView >
+    </SafeAreaView>
   );
 };
 
