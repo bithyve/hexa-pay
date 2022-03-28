2fa73d6a2c1c8b60d26ead8c3c4c110e22adf10a
PraneethGunas <praneethgunas404@gmail.com>
2022-03-29 02:24:44 +0530

    Redux Set-up

--- src/Providers.tsx
+++ src/Providers.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import {LocalizationProvider} from '~content/LocContext';
 import {NativeBaseProvider, StatusBar} from 'native-base';
 import {HexaTheme} from 'src/theme';
+import {Provider as ReduxProvider} from 'react-redux';
+import configureStore from '~store';
 
 export default function Providers({children}: {children: JSX.Element}) {
+  const store = configureStore();
+
   return (
-    <NativeBaseProvider theme={HexaTheme}>
-      <StatusBar translucent backgroundColor="transparent" barStyle={'light-content'} />
-      <LocalizationProvider>{children}</LocalizationProvider>
-    </NativeBaseProvider>
+    <ReduxProvider store={store}>
+      <NativeBaseProvider theme={HexaTheme}>
+        <StatusBar translucent backgroundColor="transparent" barStyle={'light-content'} />
+        <LocalizationProvider>{children}</LocalizationProvider>
+      </NativeBaseProvider>
+    </ReduxProvider>
   );
 }
